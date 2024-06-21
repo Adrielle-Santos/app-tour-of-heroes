9eeed274f26c056ce59a87b66108c92419e6a786
Adrielle-Santos <asds.lic21@uea.edu.br>
2024-06-21 15:21:52 -0400

    2. Display list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Homem-Aranha' },
+  { id: 3, name: 'Batman' },
+  { id: 4, name: 'Superman' },
+  { id: 5, name: 'Mulher Maravilha' },
+  { id: 6, name: 'Super-Choque' },
+  { id: 7, name: 'Flash' },
+  { id: 8, name: 'Homem de Ferro' },
+  { id: 9, name: 'Lanterna Verde' }
+];
