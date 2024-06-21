46472cbf93138b7dd76b96126fe5bf29c518c5a2
Adrielle-Santos <asds.lic21@uea.edu.br>
2024-06-21 15:43:45 -0400

    4. Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,8 @@ import {FormsModule} from '@angular/forms';
 
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +14,24 @@ import {HEROES} from '../mock-heroes';
 })
 
 export class HeroesComponent {
-  heroes = HEROES;
+
   selectedHero?: Hero;
-  
- onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
